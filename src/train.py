5e41c1ca44c392a4f88d262c06b6c4ada6f9dc87
FabricioOliveira08 <fernandesfabricio2019@gmail.com>
2026-04-21 16:20:55 -0300

    adiciona persistência do modelo e vectorizer com joblib

--- src/train.py
+++ src/train.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
+import joblib 
 
 df = pd.read_csv("data/spam.csv")
 
@@ -25,3 +26,6 @@ y_pred = model.predict(X_test)
 acuracia = accuracy_score(y_test, y_pred)
 
 print(f"Acuracia: {acuracia}")
+
+joblib.dump(model, "model/spam_model.pkl")
+joblib.dump(vectorizer, "model/vectorizer.pkl")
