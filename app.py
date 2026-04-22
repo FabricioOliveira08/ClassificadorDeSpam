c858e545ac4b6df0af7e7d7ea86ade11ab340f76
FabricioOliveira08 <fernandesfabricio2019@gmail.com>
2026-04-22 16:43:41 -0300

    atualiza a interface do Streamlit

--- app.py
+++ app.py
@@ -1,20 +1,37 @@
 import streamlit as st
 import joblib
 
+st.set_page_config(
+    page_title="Classificador de Spam",
+    layout="centered"
+)
 
 model = joblib.load("model/spam_model.pkl")
 vectorizer = joblib.load("model/vectorizer.pkl")
 
 st.title("Classificador de Spam")
+st.markdown("Digite uma mensagem abaixo e descubra se ela é **Spam ou Não Spam** usando Machine Learning.")
+
+st.divider()
 
 mensagem = st.text_input("Digite uma mensagem: ")
 
 if st.button("Classificar"):
-    X = vectorizer.transform([mensagem])        
-    resultado = model.predict(X)
 
-    st.subheader("Resultado: ")
-    if(resultado[0] == "spam"):
-        st.write("Spam")
+    if(mensagem.strip() == ""):
+        st.warning("⚠️ Digite uma mensagem antes de classificar.")
     else:
-        st.write("Não Spam")
+        X = vectorizer.transform([mensagem])        
+        resultado = model.predict(X)
+        prob = model.predict_proba(X)
+
+        st.divider()
+        st.subheader("Resultado: ")
+
+        if(resultado[0] == "spam"):
+            st.error("Spam detectado")
+        else:
+            st.success("A mensagem não é Spam")
+        
+        confianca = prob.max()
+        st.write(f" Confiança do modelo: **{confianca:.2%}**")
--- src/train.py
+++ src/train.py
@@ -12,7 +12,7 @@ y = df["label"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-vectorizer = CountVectorizer()
+vectorizer = CountVectorizer(ngram_range=(1,2))
 
 X_train = vectorizer.fit_transform(X_train)
 X_test = vectorizer.transform(X_test)
